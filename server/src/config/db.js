b2e313d722c5de2d5e75c8b89618629fce7659fe
Josh Kincheloe <jkincheloe33@gmail.com>
2018-02-26 16:06:35 -0600

    config

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,16 @@
 import mysql from 'mysql';
+let env = process.env;
+
+if (!env.DB_HOST) {
+    env = require('./config').default;
+}
 
 let pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASS,
+    database: env.DB_NAME
 });
 
 function executeQuery(sql, args = []) {
