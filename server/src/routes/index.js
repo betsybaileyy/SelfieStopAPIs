f49244ad9c967345c7a6dd6e7ffa7151bbd97296
Josh Kincheloe <jkincheloe33@gmail.com>
2018-02-27 14:19:59 -0600

    Created auth.js. Added login and signup post methods.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import peopleRouter from './people';
 import ImagesTestRouter from './imagesTest';
+import AuthRouter from './auth';
 
 let router = Router();
 
 router.use('/imagesTest', ImagesTestRouter);
+router.use('/auth', AuthRouter);
 router.use('/people', peopleRouter);
 
 export default router;
