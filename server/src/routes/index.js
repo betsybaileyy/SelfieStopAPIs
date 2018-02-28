2d0cf6940549ec5cec5edc00b06d92f3153ef1fa
Josh Kincheloe <jkincheloe33@gmail.com>
2018-02-28 12:02:43 -0600

    In table.js, we added a function that returns all locations of a specific type. In locations.js, we added a get call to the database.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,7 @@ import peopleRouter from './people';
 import ImagesTestRouter from './imagesTest';
 import AuthRouter from './auth';
 import usersRouter from './users';
+import locationsRouter from './locations';
 import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
 
 let router = Router();
@@ -13,6 +14,7 @@ router.post('*', tokenMiddleware, isLoggedIn);
 router.put('*', tokenMiddleware, isLoggedIn);
 router.delete('*', tokenMiddleware, isLoggedIn);
 
+router.use('/locations', locationsRouter);
 router.use('/imagesTest', ImagesTestRouter);
 router.use('/users', usersRouter);
 router.use('/people', peopleRouter);
