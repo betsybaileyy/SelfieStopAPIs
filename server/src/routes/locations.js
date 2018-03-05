a88106a1d880c855f4deed9a6afd148de6d717c1
Josh Kincheloe <jkinchloe33@gmail.com>
2018-03-05 12:13:56 -0600

    in images.js, edited the post method to allow a user to upload a photo without specifying a location.

--- server/src/routes/locations.js
+++ server/src/routes/locations.js
@@ -14,6 +14,7 @@ router.get('/:typeid/:id?', (req, res) => {
         locations.getAllLocations(typeid)
             .then((locations) => {
                 res.json(locations);
+                console.log();
             });
 
     } else {
