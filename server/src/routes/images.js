16caab68ad183354bcbc9a94e276063205247f8b
Josh Kincheloe <jkincheloe33@gmail.com>
2018-03-05 12:13:56 -0600

    in images.js, edited the post method to allow a user to upload a photo without specifying a location.

--- server/src/routes/images.js
+++ server/src/routes/images.js
@@ -9,30 +9,44 @@ let images = new Table('images');
 let locations = new Table('locations');
 
 router.post('/', upload.single('image'), (req, res, next) => {
+    if (!req.body.locationid) {
+        let picture = {
+            image: req.file.path,
+            userid: req.user.id,
+            locationid: 141
+        }
+        images.insert(picture)
+            .then(() => {
+                res.sendStatus(201);
+            }).catch((err) => {
+                console.log(err);
+            });
 
-    let picture = {
-        image: req.file.path,
-        userid: req.user.id,
-        locationid: req.body.locationid
+    } else {
+        let picture = {
+            image: req.file.path,
+            userid: req.user.id,
+            locationid: req.body.locationid
+        }
+
+        images.insert(picture)
+            .then(() => {
+                res.sendStatus(201);
+            }).catch((err) => {
+                console.log(err);
+            });
     }
-
-    images.insert(picture)
-        .then(() => {
-            res.sendStatus(201);
-        }).catch((err) => {
-            console.log(err);
-        });
 });
 
 router.get('/:id', (req, res) => {
     let id = req.params.id;
 
     images.getAllLocationImages(id)
-    .then((locationImages) => {
-        res.json(locationImages);
-    }).catch((err) => {
-        console.log(err);
-    });
+        .then((locationImages) => {
+            res.json(locationImages);
+        }).catch((err) => {
+            console.log(err);
+        });
 });
 
 export default router;
