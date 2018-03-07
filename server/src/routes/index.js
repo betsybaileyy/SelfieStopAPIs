0fcaf43605143cd1d31e7d88761b56e5134ddc51
Josh Kincheloe <jkincheloe33@gmail.com>
2018-03-07 09:09:05 -0600

    Fixed an issue where a user wouldn't stay logged in.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import peopleRouter from './people';
 import AuthRouter from './auth';
 import usersRouter from './users';
+import userProfileRouter from './userProfile';
 import locationsRouter from './locations';
 import imagesRouter from './images';
 import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
@@ -10,12 +11,13 @@ let router = Router();
 
 router.use('/auth', AuthRouter);
 
-// router.post('*', tokenMiddleware, isLoggedIn);
+router.post('*', tokenMiddleware, isLoggedIn);
 router.put('*', tokenMiddleware, isLoggedIn);
 router.delete('*', tokenMiddleware, isLoggedIn);
 
 router.use('/locations', locationsRouter);
 router.use('/users', usersRouter);
+router.use('/userProfile', userProfileRouter);
 router.use('/images', imagesRouter);
 router.use('/people', peopleRouter);
 
