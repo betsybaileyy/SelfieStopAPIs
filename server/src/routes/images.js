9b10ea562c75729ed3e6addc7902527fa20f98bb
Josh Kincheloe <jkinchloe33@gmail.com>
2018-03-07 14:35:38 -0600

    Trouble shooting Token authentication.

--- server/src/routes/images.js
+++ server/src/routes/images.js
@@ -38,6 +38,7 @@ router.post('/', upload.single('image'), (req, res, next) => {
                 res.sendStatus(201);
             }).catch((err) => {
                 console.log(err);
+                res.sendStatus(500);
             });
     }
 });
