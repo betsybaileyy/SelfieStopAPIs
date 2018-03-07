e7030a8a7f4714acba859f3769970370e5928324
Josh Kincheloe <jkincheloe33@gmail.com>
2018-03-07 14:35:38 -0600

    Trouble shooting Token authentication.

--- server/src/index.js
+++ server/src/index.js
@@ -25,3 +25,21 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
+// process.on('unhandledRejection', (reason, p) => {
+//     // I just caught an unhandled promise rejection, since we already have fallback
+//     // handler for unhandled errors (see below), let throw and let him handle that
+//     console.error('Unhandled Promise Rejection');
+//     console.error(reason);
+// });
+
+// process.on('uncaughtException', (error) => {
+//     // I just received an error that was never handled, time to handle it and then decide whether a restart is needed
+//     console.error('Uncaught exception');
+
+//     if (error.stack) {
+//         console.error(error.stack);
+//     } else {
+//         console.error(error.message);
+//     }
+// });
