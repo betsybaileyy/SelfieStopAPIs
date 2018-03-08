03b1608641d70902f56659e986ed1af3b91614b2
Josh Kincheloe <jkincheloe33@gmail.com>
2018-03-08 13:09:06 -0600

    Implemented AWS into our images.js. Now uploads images from the frontend to AWS

--- server/src/routes/images.js
+++ server/src/routes/images.js
@@ -1,23 +1,43 @@
 import { Router } from 'express';
 import Table from '../table';
+import AWS from 'aws-sdk';
+import multerS3 from 'multer-s3';
+import multer from 'multer';
+import params from '../config/s3config';
+import { join } from 'path';
 
-let multer = require('multer');
-let upload = multer({ dest: 'client/img/' })
-let router = Router();
 
 let images = new Table('images');
 let locations = new Table('locations');
+let router = Router();
+
+AWS.config.update(params);
+let s3 = new AWS.S3();
+
+let upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'selfiestopimages',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
+})
 
 router.post('/', upload.single('image'), (req, res, next) => {
-    console.log('Inside Images.js');
+    console.log(req.file.location);
     if (!req.body.locationid) {
         let picture = {
-            image: req.file.path,
+            image: req.file.location,
             userid: req.user.id,
             locationid: 141
         }
         images.insert(picture)
             .then(() => {
+                console.log('Insert Successful');
                 res.sendStatus(201);
             }).catch((err) => {
                 console.log(err);
